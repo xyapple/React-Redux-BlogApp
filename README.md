d7d3ccb80994c416358563098e557e47180efdbf
xyapple <ydeascentis@gmail.com>
2017-07-12 16:02:25 -0400

    Try out the BrowserRouter by create class

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# ReduxSimpleStarter
+# React-Redux-Blog-App
+
+### ReduxSimpleStarter
+```
+> npm install
+> npm start
+```
+### Steps:
+1) checkout the API
+http://reduxblog.herokuapp.com/
+
+2) set up Postman chorme extend
+
+3) install react-router-dom
+```
+npm install --save react-router-dom@4.0.0
+```
+4) understand what react-router-dom do for us in the SPA(single page application)
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
@@ -6,10 +6,37 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
+//import react-router
+import{BrowserRouter, Route} from 'react-router-dom';
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+//testing
+class Hello extends Component {
+    render(){
+        return(
+            <div>Hello!</div>
+        )
+    }
+}
+//testing
+class Goodbye extends Component {
+    render(){
+        return(
+            <div>Goodbye!</div>
+        )
+    }
+}
+
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+        <div>
+            <Route path="/hello" component={Hello}/>
+            <Route path="/goodbye" component={Goodbye}/>
+        </div>
+    </BrowserRouter>
+
   </Provider>
   , document.querySelector('.container'));
