10ce18dbbe21a1c259be3404f5c149d4c7a7136b
xyapple <ydeascentis@gmail.com>
2017-07-13 17:12:25 -0400

    Fix the error!! miss spelling action.type(action.typ)! now list of post had been render

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -2,7 +2,7 @@ import {FETCH_POSTS} from '../actions/index';
 import _ from 'lodash';
 
 export default function(state={}, action){
-    switch (action.typ) {
+    switch (action.type) {
         case FETCH_POSTS:
 
             return _.mapKeys(action.payload.data, 'id');
